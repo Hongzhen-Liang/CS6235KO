fb43e01342152e2b0e26c8b0dfb1b63904331a15
hongzhen <sinscryw@gmail.com>
2024-10-16 17:15:25 -0400

    support gpu

--- Follow2/Models/BERT/model1.py
+++ Follow2/Models/BERT/model1.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import DataLoader
 from transformers import BertForSequenceClassification, AdamW
 import pandas as pd
@@ -9,6 +10,13 @@ saveDirName = sys.argv[1]
 datasetDir = sys.argv[2]
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
 
+if torch.backends.mps.is_available(): device = "mps"
+else:device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+device = torch.device(device)
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+model = model.to(device)
+
 # data = parseData(datasetDir) # [ {"text": "I love programming", "label": 1} ]
 
 optimizer = AdamW(model.parameters(), lr=5e-5)
@@ -19,9 +27,9 @@ for epoch in range(epochs):
         chunk = chunk[['text', 'label']]
         train_loader = transformData(chunk,tokenizer)
         for batch in train_loader:
-            input_ids = batch['input_ids']
-            attention_mask = batch['attention_mask']
-            labels = batch['label']
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
 
             optimizer.zero_grad()
 
