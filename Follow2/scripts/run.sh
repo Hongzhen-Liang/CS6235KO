6d4c0a7178688059217a12482d0294697ff732a7
hongzhen <sinscryw@gmail.com>
2024-10-16 16:19:20 -0400

    debug

--- Follow2/scripts/run.sh
+++ Follow2/scripts/run.sh
@@ -1,2 +1,3 @@
+pip install -r requirements.txt
 bash train_bert_model1.sh
 bash eval_bert_model1.sh
--- Follow2/scripts/train_bert_model1.sh
+++ Follow2/scripts/train_bert_model1.sh
@@ -1,4 +1,11 @@
+echo "========start to train bert1============="
 python ../Models/BERT/model1.py ../Save_Models/bert1 ../../Datasets/NELA/9-2020.csv
+
+echo "========start to train bert2============="
 python ../Models/BERT/model1.py ../Save_Models/bert2 ../../Datasets/NELA/10-2020.csv
+
+echo "========start to train bert3============="
 python ../Models/BERT/model1.py ../Save_Models/bert3 ../../Datasets/OLD_FNC/2020-7.csv
+
+echo "========start to train bert4============="
 python ../Models/BERT/model1.py ../Save_Models/bert4 ../../Datasets/OLD_FNC/2020-8.csv
